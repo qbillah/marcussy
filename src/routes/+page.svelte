139bacdedb28d4867fcb2bcc98cdbf0d1209bf7f
Quazi Billah <billqy@farmingdale.edu>
2023-05-25 21:29:46 -0400

    first

---
+++ src/routes/+page.svelte
@@ -0,0 +1,82 @@
+<div style="width: 300px;">
+  <p>Marcussy my dearest,</p>
+  <p>Made with love is this gift for you.</p>
+  <p>
+    For those of you who have no idea what the fuck this is, check out my friend
+    Marcus aka Marcussy's photography. Here is his <a
+      href="https://www.instagram.com/marcushoots_/"
+      ><i class="las la-link" /> instagram.</a
+    >
+  </p>
+</div>
+<div id="footer">
+  <div style="display: inline-flex;">
+    <a href="https://www.instagram.com/brainmatterny/">
+      <img
+        class="logo bm"
+        src="https://fluffyfire.s3.us-east-2.amazonaws.com/bmhead1.svg"
+        alt="Brain Matter Logo"
+      />
+    </a>
+    <a href="https://www.instagram.com/yasinbillah/">
+      <img
+        class="logo yasin"
+        src="https://yds-studio.s3.us-east-2.amazonaws.com/y.svg"
+        alt="Yasin Logo"
+      />
+    </a>
+  </div>
+  <div style="display: inline-flex; text-align: right;">
+    © 2023 , <br /> Yasin & Brain
+  </div>
+</div>
+
+<style>
+  a {
+    cursor: pointer;
+    /*background-color: yellow;*/
+  }
+  #footer {
+    position: absolute;
+    width: 100vw;
+    height: 100px;
+    bottom: 0;
+    left: 0;
+    padding: 2em;
+    display: inline-flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  img.logo {
+    width: 40px;
+    height: auto;
+    filter: brightness(0);
+    padding-right: 1em;
+  }
+  img.yasin {
+    width: 45px;
+    animation: spin 10s infinite;
+    animation-delay: 2s;
+  }
+  img.bm {
+    animation: spin2 12s infinite;
+    animation-delay: 1s;
+  }
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+
+  @keyframes spin2 {
+    0% {
+      transform: rotateY(0deg);
+    }
+    100% {
+      transform: rotateY(360deg);
+    }
+  }
+</style>
